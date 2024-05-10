0a3fb05d7c6a22c13c9d18d1d41a19824b8c6d89
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-10 18:12:11 +0600

    Add Footer Section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -118,7 +118,7 @@ const Navbar = () => {
                   <img
                     title={user?.displayName}
                     alt="Tailwind CSS Navbar component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    src={user?.photoURL}
                   />
                 </div>
               </div>
