637dd94fdcfb949358a7bcc6dd0d77abc42c6962
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-10 19:18:43 +0600

    Complete Dark Light Mood

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
 import toast from "react-hot-toast";
@@ -6,6 +6,20 @@ import logo from "../../public/logo.jpg";
 
 const Navbar = () => {
   const { logOut, user } = useContext(AuthContext);
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem(theme);
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("synthwave");
+    } else {
+      setTheme("light");
+    }
+  };
   const handleLogOut = () => {
     logOut();
     toast("LogOut Successfull");
@@ -100,7 +114,44 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <div className="navbar-end">
+        <div className="navbar-end space-x-2">
+          <label className="cursor-pointer grid place-items-center">
+            <input
+              onChange={handleToggle}
+              type="checkbox"
+              value="synthwave"
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+            />
+            <svg
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+            <svg
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          </label>
           {!user && (
             <Link
               to={"/login"}
