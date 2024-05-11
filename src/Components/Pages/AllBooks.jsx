02aa556d8ba59ead0f9e9748a27216328e464871
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-11 11:45:05 +0600

    Show All data in all book components

--- src/Components/Pages/AllBooks.jsx
+++ src/Components/Pages/AllBooks.jsx
@@ -1,5 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleBook from "./SingleBook";
+
 const AllBooks = () => {
-  return <div></div>;
+  const [books, setBooks] = useState([]);
+  console.log(books);
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios("http://localhost:5000/books");
+      console.log(data);
+      setBooks(data);
+    };
+    getData();
+  }, []);
+  return (
+    <div>
+      <h1>Our All Book Here</h1>
+      <div className="grid lg:grid-cols-2 gap-5">
+        {books.map((book) => (
+          <SingleBook key={book._id} book={book}></SingleBook>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllBooks;
--- src/Components/Pages/SingleBook.jsx
+++ src/Components/Pages/SingleBook.jsx
@@ -1,18 +1,18 @@
-const SingleBook = () => {
+const SingleBook = ({ book }) => {
+  const { name, photo, description, author, rating } = book;
   return (
     <div>
-      <div className="card card-side bg-base-100 shadow-xl">
+      <div className="card card-side  bg-base-100 shadow-xl">
         <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            alt="Movie"
-          />
+          <img className="w-1/2" src={photo} alt="Movie" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
+          <h2 className="card-title">{name}</h2>
+          <h3>Author : {author}</h3>
+          <p>{description}</p>
+          <p>{rating}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
+            <button className="btn bg-lime-500">Update</button>
           </div>
         </div>
       </div>
