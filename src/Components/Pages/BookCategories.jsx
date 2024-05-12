4d1ded3961f4d646ce7bf5687ca174d8a379ac8c
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-12 10:19:56 +0600

    Add book categories section

---
+++ src/Components/Pages/BookCategories.jsx
@@ -0,0 +1,12 @@
+const BookCategories = () => {
+  return (
+    <div className="my-6">
+      <h2 className="text-xl md:text-3xl text-lime-600 font-semibold text-center">
+        Our Books
+      </h2>
+      <div></div>
+    </div>
+  );
+};
+
+export default BookCategories;
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import AboutUs from "./AboutUs";
 import Blog from "./Blog";
+import BookCategories from "./BookCategories";
 import Carousel from "./Carousel";
 import ContactUs from "./ContactUs";
 const Home = () => {
   return (
     <div>
       <Carousel></Carousel>
+      <BookCategories></BookCategories>
       <Blog></Blog>
       <AboutUs></AboutUs>
       <ContactUs></ContactUs>
