9107ec5fd26b76ebbe8e6348cb7f71186c8d5361
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-12 10:49:31 +0600

    Complete Our Book Collection section

--- src/Components/Pages/BookCategories.jsx
+++ src/Components/Pages/BookCategories.jsx
@@ -1,10 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import SingleCategories from "./SingleCategories";
+
 const BookCategories = () => {
+  const [subBooks, setSubBooks] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios(`http://localhost:5000/subBooks`);
+        console.log(data);
+        setSubBooks(data);
+      } catch (error) {
+        toast(error.message);
+      }
+    };
+    getData();
+  }, [subBooks]);
   return (
     <div className="my-6">
       <h2 className="text-xl md:text-3xl text-lime-600 font-semibold text-center">
-        Our Books
+        Our Book Collections
       </h2>
-      <div></div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-6">
+        {subBooks.slice(0, 6).map((subBook) => (
+          <SingleCategories
+            key={subBook._id}
+            subBook={subBook}
+          ></SingleCategories>
+        ))}
+      </div>
     </div>
   );
 };
